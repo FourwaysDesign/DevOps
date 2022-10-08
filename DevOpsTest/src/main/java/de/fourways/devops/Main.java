d1bc454b50b43ef619d71b394dd45de40d7a7bcb
Fourways <info@fourways.de>
2022-10-08 16:19:29 +0200

    Initial Commit

---
+++ DevOpsTest/src/main/java/de/fourways/devops/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package de.fourways.devops;
+
+/**
+ *
+ * @author sascha
+ */
+public class Main {
+    
+       public static void main(String[] args) {
+        
+           System.out.println("Welcome Home Master");
+           
+           
+    }
+    
+    
+}
